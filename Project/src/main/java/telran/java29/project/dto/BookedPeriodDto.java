db8a6b6ba0823217d005b0fb2c130dcc65f644ca
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-10-19 13:28:13 +0300

    BookPeriodDto

---
+++ Project/src/main/java/telran/java29/project/dto/BookedPeriodDto.java
@@ -0,0 +1,22 @@
+package telran.java29.project.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class BookedPeriodDto {
+	String order_id;
+    LocalDate start_date_time;
+    LocalDate end_date_time;
+    Boolean paid;
+    Double amount;
+    LocalDate booking_date;
+    UserDto person_who_booked;
+}
