1fe511ca8560790f90ee079498425dba10c1b18a
Aliaksandr Sauchanka <Aliaksandr Sauchanka@DESKTOP-CBU869C>
2019-10-19 11:45:28 +0300

    1

---
+++ Project/src/test/java/telran/java29/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.java29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
