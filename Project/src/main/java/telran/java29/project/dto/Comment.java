de8316ad19283ea0595408941b4a84c2e4661636
Aliaksandr Sauchanka <Aliaksandr Sauchanka@DESKTOP-CBU869C>
2019-10-19 13:09:22 +0300

    1

---
+++ Project/src/main/java/telran/java29/project/dto/Comment.java
@@ -0,0 +1,18 @@
+package telran.java29.project.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Comment {
+	String first_name;
+	String second_name;
+	LocalDate post_date;
+	String post;
+
+}
---
+++ Project/src/main/java/telran/java29/project/dto/OwnCar.java
@@ -0,0 +1,26 @@
+package telran.java29.project.dto;
+
+import java.util.Set;
+
+public class OwnCar {
+	String serial_number;
+    String make;
+    String model;
+    int year;
+    String engine;
+    String fuel;
+    String gear;
+    String wheels_drive;
+    int doors;
+    int seats;
+    double fuel_consumption;
+    Set<String> features;
+    String car_class;
+    Double price_per_day;
+    Double distance_included;
+    PickUpPlace pick_up_place;
+    Set<String> image_url;
+    Set<BookedPeriods> booked_periods;
+   
+
+}
