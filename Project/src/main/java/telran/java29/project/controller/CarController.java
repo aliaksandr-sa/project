0a9a4b68a6dd0a1036aa9279d5f63d1e4d26e750
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-10-26 11:25:31 +0300

    update

--- Project/src/main/java/telran/java29/project/controller/CarController.java
+++ Project/src/main/java/telran/java29/project/controller/CarController.java
@@ -1,5 +1,7 @@
 package telran.java29.project.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import telran.java29.project.domain.BookedPeriod;
 import telran.java29.project.dto.CarDto;
 import telran.java29.project.dto.NewCarDto;
 import telran.java29.project.dto.UpdateCarDto;
@@ -36,10 +39,18 @@ public class CarController {
 	@GetMapping("/car/{serial_number}")
 	public CarDto getCarById(@PathVariable String serial_number) {
 		return carService.findCarById(serial_number);
+		//TODO in implementation not to give full view of bookedPeriodDto! 
 	}
 	
+	@GetMapping("/user/cars/{serial_number}")
+	public CarDto ownerGetCarById(@PathVariable String serial_number) {
+		return carService.findOwnerCarById(serial_number);
+	}
 	
-	
+	@GetMapping("/user/cars/{serial_number}/periods")
+	public List<BookedPeriod> ownerGetBookedPeriodsByCarId(@PathVariable String serial_number){
+		return carService.findOwnerBookedPeriodsByCarId(serial_number);
+	}
 	
 	
 	
--- Project/src/main/java/telran/java29/project/service/CarService.java
+++ Project/src/main/java/telran/java29/project/service/CarService.java
@@ -1,5 +1,8 @@
 package telran.java29.project.service;
 
+import java.util.List;
+
+import telran.java29.project.domain.BookedPeriod;
 import telran.java29.project.dto.CarDto;
 import telran.java29.project.dto.NewCarDto;
 import telran.java29.project.dto.UpdateCarDto;
@@ -11,5 +14,9 @@ public interface CarService {
 	CarDto updateCar(UpdateCarDto updateCar, String serial_number);
 
 	CarDto findCarById(String serial_number);
+
+	CarDto findOwnerCarById(String serial_number);
+
+	List<BookedPeriod> findOwnerBookedPeriodsByCarId(String serial_number);
 
 }
