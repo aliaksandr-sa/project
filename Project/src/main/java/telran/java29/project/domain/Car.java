fa5106f53f4d2067721137f4ca242a51523e06fd
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-10-26 08:35:49 +0300

    car //m

--- Project/src/main/java/telran/java29/project/domain/Car.java
+++ Project/src/main/java/telran/java29/project/domain/Car.java
@@ -6,10 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 @EqualsAndHashCode(of = {"serial_number"})
+//m
 public class Car {
 	String serial_number;
     String make;
