0a0bc4b45dd99aa4b31ae4920d4b9cee963d4495
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-11-02 10:11:11 +0200

    car

--- Project/src/main/java/telran/java29/project/dao/CarRepository.java
+++ Project/src/main/java/telran/java29/project/dao/CarRepository.java
@@ -3,7 +3,6 @@ package telran.java29.project.dao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import telran.java29.project.domain.Car;
-import telran.java29.project.dto.CarDto;
 
 public interface CarRepository extends MongoRepository<Car, String> {
 	
