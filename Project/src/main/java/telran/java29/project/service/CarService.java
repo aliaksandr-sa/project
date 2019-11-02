bea2554e5532c4673c7c889ceed1a00a35848549
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-11-02 11:39:05 +0200

    carService

--- Project/src/main/java/telran/java29/project/service/CarService.java
+++ Project/src/main/java/telran/java29/project/service/CarService.java
@@ -1,15 +1,7 @@
 package telran.java29.project.service;
 
-import java.util.List;
-import java.util.Set;
-
-import telran.java29.project.domain.BookedPeriod;
 import telran.java29.project.dto.CarDto;
-import telran.java29.project.dto.CommentDto;
-import telran.java29.project.dto.ConfirmPaymentDto;
 import telran.java29.project.dto.NewCarDto;
-import telran.java29.project.dto.ReservationDto;
-import telran.java29.project.dto.ReservationResponseDto;
 import telran.java29.project.dto.UpdateCarDto;
 //m
 public interface CarService {
@@ -18,22 +10,22 @@ public interface CarService {
 
 	CarDto updateCar(UpdateCarDto updateCar, String serial_number);
 
-	CarDto findCarById(String serial_number);
-	
-	void deleteCar();
+//	CarDto findCarById(String serial_number);
 
-	CarDto findOwnerCarById(String serial_number);
+	void deleteCar(String serial_number);
 
-	List<BookedPeriod> findOwnerBookedPeriodsByCarId(String serial_number);
+//	CarDto findOwnerCarById(String serial_number);
 
-	ReservationResponseDto makeAReservation(ReservationDto reservationDto);
-
-	void makeAReservation(ConfirmPaymentDto confirmPaymentDto);
-
-	List<CarDto> get3BestBookedCars();
-
-	Set<CommentDto> getLatestComments();
-
-	void addAComment();
+//	List<BookedPeriod> findOwnerBookedPeriodsByCarId(String serial_number);
+//
+//	ReservationResponseDto makeAReservation(ReservationDto reservationDto);
+//
+//	void makeAReservation(ConfirmPaymentDto confirmPaymentDto);
+//
+//	List<CarDto> get3BestBookedCars();
+//
+//	Set<CommentDto> getLatestComments();
+//
+//	void addAComment();
 
 }
--- Project/src/main/java/telran/java29/project/service/CarServiceImpl.java
+++ Project/src/main/java/telran/java29/project/service/CarServiceImpl.java
@@ -1,203 +1,65 @@
 package telran.java29.project.service;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import telran.java29.project.convertor.Convertor;
 import telran.java29.project.dao.CarRepository;
-import telran.java29.project.dao.UserRepository;
-import telran.java29.project.domain.BookedPeriod;
 import telran.java29.project.domain.Car;
-import telran.java29.project.domain.PickUpPlace;
-import telran.java29.project.domain.User;
-import telran.java29.project.dto.BookedPeriodDto;
 import telran.java29.project.dto.CarDto;
-import telran.java29.project.dto.CommentDto;
-import telran.java29.project.dto.ConfirmPaymentDto;
 import telran.java29.project.dto.NewCarDto;
-import telran.java29.project.dto.OwnerDto;
-import telran.java29.project.dto.PickUpPlaceDto;
-import telran.java29.project.dto.ReservationDto;
-import telran.java29.project.dto.ReservationResponseDto;
 import telran.java29.project.dto.UpdateCarDto;
-import telran.java29.project.dto.UserWhoBookedDto;
+
 @Service
+//m
 public class CarServiceImpl implements CarService {
 	@Autowired
-	UserRepository userRepository;
-	@Autowired
 	CarRepository carRepository;
+	Convertor convertor;
 
 	@Override
 	public CarDto addCar(NewCarDto carDto) {
-		Car car = new Car(carDto.getSerial_number(), carDto.getMake(), carDto.getModel(), 
-				carDto.getYear(), carDto.getEngine(), carDto.getFuel(), carDto.getGear(), 
-				carDto.getWheels_drive(), carDto.getDoors(), carDto.getSeats(), 
-				carDto.getFuel_consumption(), carDto.getFeatures(), carDto.getCar_class(), 
-				carDto.getPrice_per_day(), carDto.getDistance_included(), carDto.getAbout(), 
-				convertToPickUpPlace(carDto.getPick_up_place()), carDto.getImage_url());
+		Car car = new Car(carDto.getSerial_number(), carDto.getMake(), carDto.getModel(), carDto.getYear(),
+				carDto.getEngine(), carDto.getFuel(), carDto.getGear(), carDto.getWheels_drive(), carDto.getDoors(),
+				carDto.getSeats(), carDto.getFuel_consumption(), carDto.getFeatures(), carDto.getCar_class(),
+				carDto.getPrice_per_day(), carDto.getDistance_included(), carDto.getAbout(),
+				convertor.convertToPickUpPlace(carDto.getPick_up_place()), carDto.getImage_url());
 		car = carRepository.save(car);
-		return convertToCarDto(car);
-		
+		return convertor.convertToCarDto(car);
 	}
-
 
 	@Override
 	public CarDto updateCar(UpdateCarDto updateCar, String serial_number) {
 		Car car = carRepository.findById(serial_number).get();
-		Set<String>featurs = updateCar.getFeatures();
-		if (featurs!=null) {
+		Set<String> featurs = updateCar.getFeatures();
+		if (featurs != null) {
 			featurs.forEach(car::addFeature);
 		}
-		if (updateCar.getCar_class()!=null) {
+		if (updateCar.getCar_class() != null) {
 			car.setCar_class(updateCar.getCar_class());
 		}
-		if (updateCar.getPrice_per_day()!=null) {
+		if (updateCar.getPrice_per_day() != null) {
 			car.setPrice_per_day(updateCar.getPrice_per_day());
 		}
-		if (updateCar.getDistance_included()!=null) {
+		if (updateCar.getDistance_included() != null) {
 			car.setDistance_included(updateCar.getDistance_included());
 		}
-		if (updateCar.getPick_up_place()!=null) {
-			car.setPick_up_place(convertPickupPlaceDtoToPickupPlace(updateCar.getPick_up_place()));
+		if (updateCar.getPick_up_place() != null) {
+			car.setPick_up_place(convertor.convertToPickupPlace(updateCar.getPick_up_place()));
 		}
 		Set<String> image_url = updateCar.getImage_url();
-		if (image_url!=null) {
+		if (image_url != null) {
 			image_url.forEach(car::addImageUrl);
 		}
 		carRepository.save(car);
-		return convertToCarDto(car);
-	}
-
-	public PickUpPlace convertPickupPlaceDtoToPickupPlace(PickUpPlaceDto pick_up_place) {
-		return PickUpPlace.builder()
-				.place_id(pick_up_place.getPlace_id())
-				.latitude(pick_up_place.getLatitude())
-				.longitude(pick_up_place.getLongitude())
-				.build();
-	}
-
-
-	@Override
-	public CarDto findCarById(String serial_number) {
-		// TODO Auto-generated method stub
-		return null;
+		return convertor.convertToCarDto(car);
 	}
 
 	@Override
-	public void deleteCar() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public CarDto findOwnerCarById(String serial_number) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<BookedPeriod> findOwnerBookedPeriodsByCarId(String serial_number) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ReservationResponseDto makeAReservation(ReservationDto reservationDto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void makeAReservation(ConfirmPaymentDto confirmPaymentDto) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<CarDto> get3BestBookedCars() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<CommentDto> getLatestComments() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addAComment() {
-		// TODO Auto-generated method stub
-
-	}
-
-	public CarDto convertToCarDto(Car car) {
-		return CarDto.builder()
-				.serial_number(car.getSerial_number())
-				.make(car.getMake())
-				.model(car.getModel())
-				.year(car.getYear())
-				.engine(car.getEngine())
-				.fuel(car.getFuel())
-				.gear(car.getGear())
-				.wheels_drive(car.getWheels_drive())
-				.doors(car.getDoors())
-				.seats(car.getSeats())
-				.fuel_consumption(car.getFuel_consumption())
-				.features(car.getFeatures().stream().collect(Collectors.toSet()))
-				.car_class(car.getCar_class())
-				.price_per_day(car.getPrice_per_day())
-				.distance_included(car.getDistance_included())
-				.about(car.getAbout())
-				.pick_up_place(convertPickUpPlaceToPickupPlaceDto(car.getPick_up_place()))
-				.image_url(car.getImage_url().stream().collect(Collectors.toSet()))
-				.owner(convertOwnerToOwnerDto(car.getOwner()))
-				.booked_periods(car.getBooked_periods().stream().map(this::convertBookedPeriodsToBookedPeriodDto).collect(Collectors.toSet()))
-				.build();
-	}
-
-	public BookedPeriodDto convertBookedPeriodsToBookedPeriodDto(BookedPeriod booked_period) {
-		return BookedPeriodDto.builder()
-				.order_id(booked_period.getOrder_id())
-				.start_date_time(booked_period.getStart_date_time())
-				.end_date_time(booked_period.getEnd_date_time())
-				.paid(booked_period.getPaid())
-				.amount(booked_period.getAmount())
-				.booking_date(booked_period.getBooking_date())
-				.person_who_booked(convertUserToUserWhoBookedDto(booked_period.getPerson_who_booked()))
-				.build();
-	}
-
-	public UserWhoBookedDto convertUserToUserWhoBookedDto(User person_who_booked) {
-		return UserWhoBookedDto.builder()
-				.email(person_who_booked.getEmail())
-				.first_name(person_who_booked.getFirst_name())
-				.second_name(person_who_booked.getSecond_name())
-				.phone(person_who_booked.getPhone())
-				.build();
-	}
-
-	public OwnerDto convertOwnerToOwnerDto(User owner) {
-		return OwnerDto.builder()
-			.first_name(owner.getFirst_name())
-			.second_name(owner.getSecond_name())
-			.registration_date(owner.getRegistration_date())
-			.build();
-	}
-
-	public PickUpPlaceDto convertPickUpPlaceToPickupPlaceDto(PickUpPlace pick_up_place) {
-		return PickUpPlaceDto.builder()
-				.place_id(pick_up_place.getPlace_id())
-				.latitude(pick_up_place.getLatitude())
-				.longitude(pick_up_place.getLatitude())
-				.build();
-	}
-
-	public PickUpPlace convertToPickUpPlace(PickUpPlaceDto pick_up_place) {
-		return new PickUpPlace(pick_up_place.getPlace_id(), pick_up_place.getLatitude(), pick_up_place.getLongitude());
+	public void deleteCar(String serial_number) {
+		Car car = carRepository.findById(serial_number).get();
+		carRepository.delete(car);
 	}
 }
