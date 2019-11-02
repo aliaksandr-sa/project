686427789595ad85bbee8fdc1af10e4cd393b560
Aliaksandr Sauchanka <Aliaksandr Sauchanka@DESKTOP-CBU869C>
2019-11-02 11:41:26 +0200

    1

--- Project/src/main/java/telran/java29/project/service/FindService.java
+++ Project/src/main/java/telran/java29/project/service/FindService.java
@@ -1,13 +1,11 @@
 package telran.java29.project.service;
 
-import java.util.List;
-
-import telran.java29.project.domain.BookedPeriod;
+import telran.java29.project.dto.BookedPeriodDto;
 import telran.java29.project.dto.CarDto;
 //S
 public interface FindService {
 	
 	CarDto getCarById(String serial_number);
 
-	List<BookedPeriod> ownerGetBookedPeriodsByCarId(String serial_number);
+	Iterable<BookedPeriodDto> ownerGetBookedPeriodsByCarId(String serial_number);
 }
--- Project/src/main/java/telran/java29/project/service/FindServiceImpl.java
+++ Project/src/main/java/telran/java29/project/service/FindServiceImpl.java
@@ -1,16 +1,13 @@
 package telran.java29.project.service;
 
-import java.util.List;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import telran.java29.project.convertor.Convertor;
 import telran.java29.project.dao.CarRepository;
 import telran.java29.project.dao.UserRepository;
-import telran.java29.project.domain.BookedPeriod;
 import telran.java29.project.domain.Car;
+import telran.java29.project.dto.BookedPeriodDto;
 import telran.java29.project.dto.CarDto;
 
 @Service
@@ -20,6 +17,7 @@ public class FindServiceImpl implements FindService {
 	@Autowired
 	CarRepository carRepository;
 	Convertor convertor;
+	
 	@Override
 	public CarDto getCarById(String serial_number) {
 		Car car = carRepository.findById(serial_number).get();
@@ -27,9 +25,8 @@ public class FindServiceImpl implements FindService {
 	}
 
 	@Override
-	public List<BookedPeriod> ownerGetBookedPeriodsByCarId(String serial_number) {
-		Set<BookedPeriod> bookedPeriods = carRepository.findById(serial_number).get().getBooked_periods();
-		return null;
+	public Iterable<BookedPeriodDto> ownerGetBookedPeriodsByCarId(String serial_number) {
+		return carRepository.findBooked_periodsBySerial_number(serial_number)
 	}
 
 }
