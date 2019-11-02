e30bc99866b1c5aed2762f314b77b819ee1a4b47
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-11-02 09:57:36 +0200

    update cars

--- Project/src/main/java/telran/java29/project/domain/Car.java
+++ Project/src/main/java/telran/java29/project/domain/Car.java
@@ -1,19 +1,19 @@
 package telran.java29.project.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.data.annotation.Id;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
-@Setter
+
 @EqualsAndHashCode(of = {"serial_number"})
 //m
 public class Car {
@@ -36,6 +36,34 @@ public class Car {
     String about;
     PickUpPlace pick_up_place;
     Set<String> image_url;
+    @Setter
     User owner;
+    @Setter
     Set<BookedPeriod> booked_periods;
+    
+	public Car(String serial_number, String make, String model, int year, String engine, String fuel, String gear,
+			String wheels_drive, int doors, int seats, Double fuel_consumption, Set<String> features, String car_class,
+			Double price_per_day, Double distance_included, String about, PickUpPlace pick_up_place,
+			Set<String> image_url) {
+		this.serial_number = serial_number;
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.engine = engine;
+		this.fuel = fuel;
+		this.gear = gear;
+		this.wheels_drive = wheels_drive;
+		this.doors = doors;
+		this.seats = seats;
+		this.fuel_consumption = fuel_consumption;
+		this.features = features;
+		this.car_class = car_class;
+		this.price_per_day = price_per_day;
+		this.distance_included = distance_included;
+		this.about = about;
+		this.pick_up_place = pick_up_place;
+		this.image_url = new HashSet<>();
+		this.owner = null;
+		this.booked_periods = new HashSet<>();
+	}
 }
---
+++ Project/src/main/java/telran/java29/project/service/CarServiceImpl.java
@@ -0,0 +1,150 @@
+package telran.java29.project.service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import telran.java29.project.dao.CarRepository;
+import telran.java29.project.dao.UserRepository;
+import telran.java29.project.domain.BookedPeriod;
+import telran.java29.project.domain.Car;
+import telran.java29.project.domain.PickUpPlace;
+import telran.java29.project.domain.User;
+import telran.java29.project.dto.BookedPeriodDto;
+import telran.java29.project.dto.CarDto;
+import telran.java29.project.dto.CommentDto;
+import telran.java29.project.dto.ConfirmPaymentDto;
+import telran.java29.project.dto.NewCarDto;
+import telran.java29.project.dto.OwnerDto;
+import telran.java29.project.dto.PickUpPlaceDto;
+import telran.java29.project.dto.ReservationDto;
+import telran.java29.project.dto.ReservationResponseDto;
+import telran.java29.project.dto.UpdateCarDto;
+
+public class CarServiceImpl implements CarService {
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	CarRepository carRepository;
+
+	@Override
+	public CarDto addCar(NewCarDto carDto) {
+		Car car = new Car(carDto.getSerial_number(), carDto.getMake(), carDto.getModel(), 
+				carDto.getYear(), carDto.getEngine(), carDto.getFuel(), carDto.getGear(), 
+				carDto.getWheels_drive(), carDto.getDoors(), carDto.getSeats(), 
+				carDto.getFuel_consumption(), carDto.getFeatures(), carDto.getCar_class(), 
+				carDto.getPrice_per_day(), carDto.getDistance_included(), carDto.getAbout(), 
+				convertToPickUpPlace(carDto.getPick_up_place()), carDto.getImage_url());
+		car = carRepository.save(car);
+		return convertToCarDto(car);
+	}
+
+	private CarDto convertToCarDto(Car car) {
+		return CarDto.builder()
+				.serial_number(car.getSerial_number())
+				.make(car.getMake())
+				.model(car.getModel())
+				.year(car.getYear())
+				.engine(car.getEngine())
+				.fuel(car.getFuel())
+				.gear(car.getGear())
+				.wheels_drive(car.getWheels_drive())
+				.doors(car.getDoors())
+				.seats(car.getSeats())
+				.fuel_consumption(car.getFuel_consumption())
+				.features(car.getFeatures().stream().collect(Collectors.toSet()))
+				.car_class(car.getCar_class())
+				.price_per_day(car.getPrice_per_day())
+				.distance_included(car.getDistance_included())
+				.about(car.getAbout())
+				.pick_up_place(convertPickUpPlaceToPickupPlaceDto(car.getPick_up_place()))
+				.image_url(car.getImage_url().stream().collect(Collectors.toSet()))
+				.owner(convertOwnerToOwnerDto(car.getOwner()))
+				.booked_periods(car.getBooked_periods().stream().map(this::convertBookedPeriodsToBookedPeriodDto).collect(Collectors.toSet()))
+				.build();
+	}
+
+	private BookedPeriodDto convertBookedPeriodsToBookedPeriodDto(BookedPeriod booked_periods) {
+		return null;
+	}
+
+	private OwnerDto convertOwnerToOwnerDto(User owner) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private PickUpPlaceDto convertPickUpPlaceToPickupPlaceDto(PickUpPlace pick_up_place) {
+		return PickUpPlaceDto.builder()
+				.place_id(pick_up_place.getPlace_id())
+				.latitude(pick_up_place.getLatitude())
+				.longitude(pick_up_place.getLatitude())
+				.build();
+	}
+
+	private PickUpPlace convertToPickUpPlace(PickUpPlaceDto pick_up_place) {
+		return new PickUpPlace(pick_up_place.getPlace_id(), pick_up_place.getLatitude(), pick_up_place.getLongitude());
+	}
+
+	@Override
+	public CarDto updateCar(UpdateCarDto updateCar, String serial_number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CarDto findCarById(String serial_number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteCar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public CarDto findOwnerCarById(String serial_number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BookedPeriod> findOwnerBookedPeriodsByCarId(String serial_number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReservationResponseDto makeAReservation(ReservationDto reservationDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void makeAReservation(ConfirmPaymentDto confirmPaymentDto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<CarDto> get3BestBookedCars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<CommentDto> getLatestComments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addAComment() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
