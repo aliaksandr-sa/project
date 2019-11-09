b0ecf986b797896da16d3f2f2e3ea9e8e6992903
SyMS <SyMS@na1r.services.adobe.com>
2019-11-09 11:36:36 +0200

    asf

--- Project/src/main/java/telran/java29/project/dao/CarRepository.java
+++ Project/src/main/java/telran/java29/project/dao/CarRepository.java
@@ -7,7 +7,7 @@ import telran.java29.project.dto.BookedPeriodDto;
 
 public interface CarRepository extends MongoRepository<Car, String> {
 
-	Iterable<BookedPeriodDto> findBooked_periodsBySerial_number(String serial_number);
+//	Iterable<BookedPeriodDto> findBooked_periodsBySerial_number(String serial_number);
 	
 //	CarDto findCarById(String serial_number);
 }
