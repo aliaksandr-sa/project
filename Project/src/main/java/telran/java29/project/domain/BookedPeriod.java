5dae73892529b7627b5b2f2cdb5568c1a1efa41c
Aliaksandr Sauchanka <Aliaksandr Sauchanka@DESKTOP-CBU869C>
2019-11-09 09:02:00 +0200

    1

--- Project/src/main/java/telran/java29/project/domain/BookedPeriod.java
+++ Project/src/main/java/telran/java29/project/domain/BookedPeriod.java
@@ -1,6 +1,6 @@
 package telran.java29.project.domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
 
@@ -22,12 +22,12 @@ public class BookedPeriod {
 	@Id
 	String order_id;
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-    LocalDate start_date_time;
+    LocalDateTime start_date_time;
 	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-    LocalDate end_date_time;
+    LocalDateTime end_date_time;
     Boolean paid;
     Double amount;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-    LocalDate booking_date;
+    LocalDateTime booking_date;
     User person_who_booked;
 }
