60445f427c6da34ee026d0c606b111a4fa20e201
Aliaksandr Sauchanka <Aliaksandr Sauchanka@DESKTOP-CBU869C>
2019-11-09 09:03:26 +0200

    1

--- Project/src/main/java/telran/java29/project/dto/BookedPeriodDto.java
+++ Project/src/main/java/telran/java29/project/dto/BookedPeriodDto.java
@@ -1,6 +1,6 @@
 package telran.java29.project.dto;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,10 +13,10 @@ import lombok.NoArgsConstructor;
 
 public class BookedPeriodDto {
 	String order_id;
-    LocalDate start_date_time;
-    LocalDate end_date_time;
+    LocalDateTime start_date_time;
+    LocalDateTime end_date_time;
     Boolean paid;
     Double amount;
-    LocalDate booking_date;
+    LocalDateTime booking_date;
     UserWhoBookedDto person_who_booked;
 }
