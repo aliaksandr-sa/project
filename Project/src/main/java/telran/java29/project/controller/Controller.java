84b30ea3db17c8ac17523692f884b0bbfea91035
Aliaksandr Sauchanka <Aliaksandr Sauchanka@DESKTOP-CBU869C>
2019-11-23 11:05:18 +0200

    1

--- Project/src/main/java/telran/java29/project/controller/Controller.java
+++ Project/src/main/java/telran/java29/project/controller/Controller.java
@@ -101,7 +101,6 @@ public class Controller {
 		return findService.ownerGetBookedPeriodsByCarId(serial_number);
 	}
 	
-
 //	@GetMapping("/search?country=string&city=string&start_date="YYYY-MM-dd HH:mm"&end_date="YYYY-MM-dd HH:mm"&ascending=true&min_amount=20.5&max_amount=35.5")
 //	public List<CarDto> search car by place and start/end dates(???){
 //		return carService.searchWithoutFilters(???);
