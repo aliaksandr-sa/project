3d95c806c116d06c57eab6c0b7d6d22cd9c1a94d
SyMS <SyMS@DESKTOP-AN7MMDO>
2019-12-03 20:37:06 +0200

    asdad

--- Project/src/main/java/telran/java29/project/dao/CarRepository.java
+++ Project/src/main/java/telran/java29/project/dao/CarRepository.java
@@ -1,9 +1,15 @@
 package telran.java29.project.dao;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import telran.java29.project.domain.Car;
+import telran.java29.project.dto.SearchResultDto;
 
 public interface CarRepository extends MongoRepository<Car, String> {
+
+	Iterable<SearchResultDto> findByLocationNear(Point point, Distance distance, Pageable pageable);
 	
 }
